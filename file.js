004319f2af985449a7a20f5c7739daa8aa504eeb
Vinicius Passos <viniciuspa87@hotmail.com>
2024-04-22 20:08:06 -0300

    style: change double brackets

--- file.js
+++ file.js
@@ -1,13 +1,13 @@
-const fs = require('fs');
-const conteudo = { 
-  title: "Título", 
-  content: 'Conteúdo a ser gravado no arquivo'
+const fs = require("fs");
+const conteudo = {
+  title: "Título",
+  content: "Conteúdo a ser gravado no arquivo",
 };
 
-fs.writeFile('arquivo2.txt', JSON.stringify(conteudo, null, 2), (err) => {
+fs.writeFile("arquivo2.txt", JSON.stringify(conteudo, null, 2), (err) => {
   if (err) {
     console.error(err);
     return;
   }
-  console.log('Arquivo gravado com sucesso!');
+  console.log("Arquivo gravado com sucesso!");
 });
